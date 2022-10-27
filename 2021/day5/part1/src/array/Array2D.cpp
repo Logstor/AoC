bf3c7ed72487bc27d2b7d2ff963e72b29ffab675
Logstor <a.rottger_rydahl@live.dk>
2022-10-27 15:57:58 +0200

    Added Purgato to CMakelists

--- 2021/day5/part1/src/array/Array2D.cpp
+++ 2021/day5/part1/src/array/Array2D.cpp
@@ -6,8 +6,9 @@ namespace Purgato
 {
     template<typename T>
     Array2D<T>::Array2D(const unsigned int rows, const unsigned int cols)
-        : rows(rows), cols(cols), length(rows * cols)
+        : rows(rows), cols(cols)
     {
+        this->length = rows * cols;
         this->data = static_cast<T*>( malloc(sizeof(T), this->length) );
     }
 
