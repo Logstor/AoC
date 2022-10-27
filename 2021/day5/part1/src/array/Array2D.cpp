054df43ff918538bcb1c3cc874c2c3a2738c8144
Logstor <a.rottger_rydahl@live.dk>
2022-10-27 15:55:55 +0200

    Implemented Array2D

---
+++ 2021/day5/part1/src/array/Array2D.cpp
@@ -0,0 +1,16 @@
+#include "Purgato.h"
+
+#include <stdlib.h>
+
+namespace Purgato
+{
+    template<typename T>
+    Array2D<T>::Array2D(const unsigned int rows, const unsigned int cols)
+        : rows(rows), cols(cols), length(rows * cols)
+    {
+        this->data = static_cast<T*>( malloc(sizeof(T), this->length) );
+    }
+
+    template<typename T>
+    Array2D<T>::~Array2D() { free(this->data); }
+};
